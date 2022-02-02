5a88ac02ac98fffcdcf673546fadf1eb0d7fc6aa
Commandrator <62714720+Commandrator@users.noreply.github.com>
2022-02-02 23:29:04 +0300

    Create HC-SR04 Mesafe Sensörü.py

---
+++ HC-SR04 Mesafe Sensörü.py
@@ -0,0 +1,22 @@
+from machine import Pin
+from machine import time_pulse_us
+import utime
+trigger = Pin(18, Pin.OUT)
+echo = Pin(21, Pin.IN)
+def echoTime():
+    trigger.value(0)
+    utime.sleep_us(5)
+    trigger.value(1)
+    utime.sleep_us(10)
+    trigger.value(0)
+    try:
+        echoTime = time_pulse_us(echo, 1, 1000000)
+        cm = (echoTime * 0.034321) * 0.5
+
+        print(str(cm)+" cm")
+    except OSError as e:
+        print(e)
+
+while True:
+    echoTime()
+    utime.sleep(1)
